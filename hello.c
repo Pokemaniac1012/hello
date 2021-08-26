23fb63fc56c088d48f38cf8de29139921d6847bf
Christian Tyndall <ctyndall2@huskers.unl.edu>
2021-08-26 19:31:53 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Christian Tyndall
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Hello, my name is Christian Tyndall! \n");
+     printf("My major is Computer Science! \n");
+
+     return 0;
+ }
